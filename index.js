2369a7e5b8730a1f5f89404e706e0fc20d640ed9
Anna Ruiz <45431125+lilhappyburrito@users.noreply.github.com>
2019-11-13 09:58:32 -0700

    const set-up

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const express = require('express'),
-    pug = require('pug'),
-    path = require('path'),
-    route = require('./routes/routes.js'),
-    bodyParser = require('body-parser'),
-    cookieParser = require('cookie-parser');
+const express = require('express');
+const pug = require('pug');
+const path = require('path');
+const route = require('./routes/routes.js');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const expressSessions = require('express-sessions');
 
 const app = express();
 
 app.set('view engine', 'pug');
-app.set('views', __dirname+'/views');
+app.set('views', __dirname + '/views');
 
-app.use(express.static(path.join(__dirname+'/public')));
+app.use(express.static(path.join(__dirname + '/public')));
 app.use(cookieParser('passphrase that we need to change later'));
 
 var urlencodedParser = bodyParser.urlencoded({
-    extended: true
+	extended: true
 });
 
-
+app.get('/', route.index);
 
 app.listen(3000);
--- routes/routes.js
+++ routes/routes.js
@@ -9,8 +9,23 @@ mdb.once('open', function (callback) {
 });
 
 var userSchema = mongoose.Schema({
-    username: String,
-    firstName: String,
-    lastName: String,
-    age: String
+	username: String,
+	password: String,
+	email: String,
+	age: String,
+	ques1Answer: String,
+	ques2Answer: String,
+	ques3Answer: String
 });
+
+var User = mongoose.model('User_Collection', userSchema);
+
+exports.index = (req, res) => {
+	User.find(function (err, user) {
+		if (err) return console.error(err);
+		res.render('index', {
+			title: 'Users',
+			user: user
+		});
+	});
+};
