528b643766b4cad0e5c6fd727d07e46b02eec9dd
Anna Ruiz <45431125+lilhappyburrito@users.noreply.github.com>
2019-12-04 05:06:33 -0700

    Graph attempt

--- public/home.js
+++ public/home.js
@@ -3,13 +3,38 @@
 //label the graph
 //may use external library for picking colors
 
-var height;
-var width;
 var canvas = document.getElementById("onlyChildGraph");
-var ctx = canvas.getContext("2d");
-ctx.fillStyle = "#99434c";
-// ctx.fillRect(0, 0, 150, 75);
+var childCtx = canvas.getContext("2d");
+childCtx.fillStyle = "#99434c";
 
-ctx.beginPath();
+function drawLine(ctx, startX, startY, endX, endY, color) {
+  ctx.save();
+  ctx.strokeStyle = color;
+  ctx.beginPath();
+  ctx.moveTo(startX, startY);
+  ctx.lineTo(endX, endY);
+  ctx.stroke();
+  ctx.restore();
+}
 
-ctx.fillText("Number of Answers", 0, 20);
+function drawBar(ctx, x, y, width, height, color) {
+  ctx.save();
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, width, height);
+  ctx.restore();
+}
+
+function drawGraph(ctx, yesHeight, noHeight) {
+  yesHeight = yesHeight * 10;
+  noHeight = noHeight * 10;
+
+  ctx.font = "15px Arial";
+  drawBar(ctx, 0, 70, 50, yesHeight, '#99434c');
+  ctx.fillText("Yes", 15, 60);
+  drawBar(ctx, 60, 70, 50, noHeight, '#99434c');
+  ctx.fillText("No", 75, 60);
+  ctx.fillText("12", 15, yesHeight + 90);
+  ctx.fillText("12", 75, noHeight + 90);
+}
+
+drawGraph(childCtx, 12, 15);
